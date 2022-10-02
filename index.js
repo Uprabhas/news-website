634f25ff8148bc712fd36c1d1dfb6f04af64c16f
prabhas <111869609+Uprabhas@users.noreply.github.com>
2022-10-02 16:57:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const newsdetails = document.getElementById("newsdetails");
 var newsDataArr = [];
 
 // apis 
-const API_KEY = "2cd2f2a198e6488e8a68df153b93baf3";
+const API_KEY = "write your api-key in this place";
 const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=in&apiKey=";
 const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=";
 const BUSINESS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=";
